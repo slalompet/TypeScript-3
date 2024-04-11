2e7c7163cfdd36c1d9e1d51000bd142d475e9257
Yao <yao_yu.wu@futuregames.nu>
2024-04-11 15:37:50 +0200

    Router link
    - modified mainmenu component and changed anchor tag from href to routerlink

--- my-angular-app/src/app/mainmenu/mainmenu.component.ts
+++ my-angular-app/src/app/mainmenu/mainmenu.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-mainmenu',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './mainmenu.component.html',
   styleUrl: './mainmenu.component.scss'
 })
