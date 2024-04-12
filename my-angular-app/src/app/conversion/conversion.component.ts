df2dc9b6292be258dcbcf47c80f0130de24eab81
Yao <yao_yu.wu@futuregames.nu>
2024-04-12 15:32:32 +0200

    Conversion component
    - added array of numbers to convert temperature

--- my-angular-app/src/app/conversion/conversion.component.ts
+++ my-angular-app/src/app/conversion/conversion.component.ts
@@ -1,12 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-conversion',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './conversion.component.html',
   styleUrl: './conversion.component.scss'
 })
 export class ConversionComponent {
 
+  // convert celcius to fahrenheit in an array
+
+  tempArr: number[] = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+
+  constructor() { }
+
+  addFahrenheit(celcius: number): number {
+    return celcius * 9 / 5 + 32;
+  }
 }
